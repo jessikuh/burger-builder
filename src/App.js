012067cc9f320ab5316a24ea7ee153bb4ddad56d
Jessica Stamos <jessica@jessicastamos.com>
2019-12-15 12:32:23 -0600

    BurgerBuilder container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import Layout from './components/Layout/Layout';
+import Layout from './components/Layout';
+import BurgerBuilder from './containers/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>
-          Test.
-        </p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
