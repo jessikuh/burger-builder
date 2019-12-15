72b011b610a12547989f62a4ff8ca5126dd92139
Jessica Stamos <jessica@jessicastamos.com>
2019-12-15 16:29:33 -0600

    Build Controls start

---
+++ src/components/Burger/BuildControls/BuildControl/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './BuildControl.module.scss';
+
+const buildControl = ({ label }) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More}>More</button>
+  </div>
+);
+
+export default buildControl;
+
---
+++ src/components/Burger/BuildControls/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './BuildControls.module.scss';
+
+const buildControls = (props) => (
+  <div>
+
+  </div>
+);
+
+export default buildControls;
+
