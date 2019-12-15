f8178762c63870b9bb419b718d3620552064faad
Jessica Stamos <jessica@jessicastamos.com>
2019-12-15 12:33:23 -0600

    Quick styling

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 
+import classes from './Layout.module.scss';
 
 const layout = ({ children }) => (
   <Fragment>
     <div>
       Toolbar, SideDrawer, Backdrop
     </div>
-    <main>
+    <main className={classes.content}>
       {children}
     </main>
   </Fragment>
