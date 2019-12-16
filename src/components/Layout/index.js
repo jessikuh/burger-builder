ee2ed36dac0d586ff89f941ffda5e71eff641ca3
Jessica Stamos <jessica@jessicastamos.com>
2019-12-15 18:24:33 -0600

    Added toolbar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 
 import classes from './Layout.module.scss';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = ({ children }) => (
   <Fragment>
     <div>
-      Toolbar, SideDrawer, Backdrop
+      <Toolbar />
     </div>
     <main className={classes.content}>
       {children}
