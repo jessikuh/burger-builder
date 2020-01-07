e5112a6c5dad2a161c902823f3167f02ffc4e931
Jessica Stamos <jstamos@envano.com>
2020-01-07 08:48:36 -0600

    Fetch orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-export default class Orders extends Component {
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then((response) => {
+        const fetchedOrders = [];
+
+        Object.keys(response.data).forEach((key) => {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key,
+          });
+        });
+
+        console.log(fetchedOrders);
+
+        this.setState({
+          loading: false,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          loading: false,
+        });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -12,3 +44,5 @@ export default class Orders extends Component {
     );
   }
 }
+
+export default withErrorHandler(Orders, axios);
