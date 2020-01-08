b23401463c79ee32bb33f50a72f1fec97eb6310f
Jessica Stamos <jstamos@envano.com>
2020-01-08 09:50:12 -0600

    Creating select dropdown, and passing correct attributes

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'react/jsx-filename-extension': 0,
     'react/prefer-stateless-function': 0,
     'react/state-in-constructor': 0,
-    'jsx-a11y/label-has-associated-control': 0
+    'jsx-a11y/label-has-associated-control': 0,
+    'react/no-unused-state': 0,
   },
 };
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,70 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          name: 'name',
+          placeholder: 'Your Name',
+        },
+        label: 'Your Name',
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          name: 'email',
+          placeholder: 'Your Email',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          name: 'street',
+          placeholder: 'Your Street',
+        },
+        value: '',
+      },
+      postalCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          name: 'postal',
+          placeholder: 'Your Postal Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          name: 'country',
+          placeholder: 'Your Country',
+        },
+        label: 'Your Country',
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {
+              value: 'fastest',
+              displayValue: 'Fastest',
+            },
+            {
+              value: 'cheapest',
+              displayValue: 'Cheapest',
+            },
+          ],
+        },
+        value: '',
+      },
     },
     loading: false,
     purchasing: false,
@@ -55,12 +114,26 @@ class ContactData extends Component {
   };
 
   render() {
+    const formElementsArray = [];
+
+    Object.keys(this.state.orderForm).forEach((key) => {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key],
+      });
+    });
+
     let form = (
       <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-        <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
-        <Input inputtype="input" type="text" name="postal" placeholder="Your Postal Code" />
+        {formElementsArray.map((formElement) => (
+          <Input
+            key={formElement.id}
+            label={formElement.config.label}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
         <Button
           btnType="Success"
           clicked={this.orderHandler}
