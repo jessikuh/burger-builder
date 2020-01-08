130fc26c91bc979aa1dfc8c38f8c6ee51350a113
Jessica Stamos <jstamos@envano.com>
2020-01-08 10:31:28 -0600

    Styling for form invalid

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -23,6 +23,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       email: {
         elementType: 'input',
@@ -37,6 +38,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       street: {
         elementType: 'input',
@@ -51,6 +53,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       postalCode: {
         elementType: 'input',
@@ -67,6 +70,7 @@ class ContactData extends Component {
           minLength: 5,
         },
         valid: false,
+        touched: false,
       },
       country: {
         elementType: 'input',
@@ -81,6 +85,7 @@ class ContactData extends Component {
           required: true,
         },
         valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: 'select',
@@ -146,10 +151,9 @@ class ContactData extends Component {
     updatedFormElement.value = event.target.value;
 
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
 
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-
-    console.log(updatedFormElement);
 
     this.setState({
       orderForm: updatedOrderForm,
@@ -195,6 +199,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={this.inputChangedHandler(formElement.id)}
           />
         ))}
